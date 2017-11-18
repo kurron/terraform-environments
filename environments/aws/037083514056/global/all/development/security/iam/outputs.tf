159beeb232c3fd741d89200e660249cac595813e
Ron Kurr <kurr@kurron.org>
2017-11-18 15:12:03 -0500

    Fresh slate

--- environments/aws/037083514056/global/all/development/security/iam/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "cross_account_ecr_pull_profile_id" {
-    value = "${module.iam.cross_account_ecr_pull_profile_id}"
-}
-
-output "ec2_park_role_arn" {
-    value = "${module.iam.ec2_park_role_arn}"
-}
--- environments/aws/037083514056/us-east-1/all/development/compute/security-groups/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "bastion_id" {
-    value = "${module.security-group.bastion_id}"
-}
-
-output "ec2_id" {
-    value = "${module.security-group.ec2_id}"
-}
